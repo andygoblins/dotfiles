52aaaa1c173babd26e5c85b34d78701ef9449e3d
Andrew <thisismyfriendsemailaddress@gmail.com>
2013-06-09 12:42:31 -0500

    added files from windows branch

---
+++ .goblinkeys.zsh
@@ -0,0 +1,69 @@
+# implement goblinkeys for zsh
+
+# disable ^U in stty
+stty kill undef
+
+# start (almost) tabula rasa
+bindkey -N goblin .safe
+
+# insert traditional terminal things
+bindkey -M goblin "^V" quoted-insert
+bindkey -M goblin "^I" expand-or-complete
+bindkey -M goblin "^[s" spell-word
+bindkey -M goblin "^[g" send-break
+
+# navigation
+bindkey -M goblin "^[[1;5l" beginning-of-line #ctl-, in mintty
+bindkey -M goblin "^[[H" beginning-of-line #home
+bindkey -M goblin "^[," beginning-of-history
+bindkey -M goblin "^[[1;3H" beginning-of-history #alt-home
+bindkey -M goblin "^P" end-of-line
+bindkey -M goblin "^[[F" end-of-line #end
+bindkey -M goblin "^[p" end-of-history
+bindkey -M goblin "^[[1;3F" end-of-history #alt-end
+bindkey -M goblin "^O" backward-char
+bindkey -M goblin "^[[D" backward-char #left
+bindkey -M goblin "^[o" backward-word
+bindkey -M goblin "^[[1;5D" backward-word #ctl-left
+bindkey -M goblin "^U" forward-char
+bindkey -M goblin "^[[C" forward-char #right
+bindkey -M goblin "^[u" forward-word
+bindkey -M goblin "^[[1;5C" forward-word #ctl-right
+bindkey -M goblin "^E" down-line-or-history
+bindkey -M goblin "^[[B" down-line-or-history #down
+bindkey -M goblin "^[e" end-of-history
+bindkey -M goblin "^[[1;5B" end-of-history #ctl-down
+bindkey -M goblin "^[[1;5n" up-line-or-history #ctl-. in mintty
+bindkey -M goblin "^[[A" up-line-or-history #up
+bindkey -M goblin "^[." beginning-of-history
+bindkey -M goblin "^[[1;5A" beginning-of-history #ctl-up
+
+# deletions
+bindkey -M goblin "^D" delete-char-or-list
+bindkey -M goblin "^[[3~" delete-char-or-list #delete
+bindkey -M goblin "^[d" delete-word
+bindkey -M goblin "^[[3;5~" delete-word #ctl-delete
+bindkey -M goblin "^?" backward-delete-char
+bindkey -M goblin "^_" backward-delete-word #ctl-bs in mintty
+
+# copy/paste
+bindkey -M goblin "^G" kill-region
+bindkey -M goblin "^R" yank
+bindkey -M goblin "^[r" yank-pop
+bindkey -M goblin "^K" kill-line
+bindkey -M goblin "^@" set-mark-command
+
+# searching
+bindkey -M goblin "^F" history-incremental-search-forward
+bindkey -M goblin "^[f" history-incremental-search-backward
+
+# text
+bindkey -M goblin "^[c" up-case-word
+bindkey -M goblin "^L" down-case-word
+bindkey -M goblin "^T" transpose-chars
+bindkey -M goblin "^[t" transpose-words
+
+# misc
+bindkey -M goblin "^Z" undo
+bindkey -M goblin "^[z" redo
+bindkey -M goblin "^H" run-help
---
+++ .zshrc
@@ -0,0 +1,56 @@
+# prompt
+PROMPT='%(0?.%F{green}✓.%F{red}✘)%F{red}▐%f%B%K{red} %n@%m %~%F{black}░▒%b%k%f'
+#autoload -U colors && colors
+#PROMPT="%(0?.%{$fg_no_bold[green]%}✓.%{fg_no_bold[red]%}✘)%{$fg_no_bold[red]%}▐%{$reset_color%}%{$bg[red]%} %n@%m %~%{$fg[black]%}░▒%{$reset_color%}"
+
+# history
+setopt APPEND_HISTORY HIST_FIND_NO_DUPS
+HISTFILE="${HOME}/.zsh_history"
+HISTSIZE='1000'
+SAVEHIST="${HISTSIZE}"
+
+# variables
+export EDITOR="/usr/bin/nano"
+export PATH="$PATH:~/john/run"
+# basic ls colors
+export LS_COLORS="di=01;34:fi=0:ln=01;36:pi=40;33:so=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01"
+# archive ls colors
+export LS_COLORS="$LS_COLORS:*.tar=33:*.tgz=33:*.tbz=33:*.tbz2=33:*.tb2=33:*.tlz=33:*.txz=33:*.gz=33:*.bz2=33:*.xz=33:*.lz=33:*.lzma=33:*.7z=33"
+
+# aliases
+alias ls='ls --color=auto'
+alias ll="ls -l"
+alias la="ls -la"
+
+# keyboard
+source "${HOME}/.goblinkeys.zsh"
+bindkey -A goblin main
+bindkey -M goblin ' ' magic-space
+bindkey -M goblin '^^' push-line-or-edit #ctl-enter
+
+# completions
+zstyle ':completion:*' completer _expand _complete _ignored _correct _approximate _prefix
+zstyle ':completion:*' expand suffix
+zstyle ':completion:*' ignore-parents parent
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+# when in Windows, do as DOS does (case-insensitive matching)
+zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}'
+zstyle ':completion:*' max-errors 2 numeric
+zstyle ':completion:*' menu select=long
+zstyle ':completion:*' preserve-prefix '//[^/]##/'
+zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+zstyle ':completion:*' squeeze-slashes true
+zstyle :compinstall filename '/cygdrive/c/Users/caud355/.zshrc'
+
+autoload -Uz compinit && compinit
+
+# misc
+setopt AUTOLIST COMPLETE_IN_WORD # make completions behave
+setopt CORRECT DVORAK # spellcheck
+setopt ALIASES # expand aliases
+
+# ssh-only options
+if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+	setopt IGNORE_EOF # don't logout with an accidental ctl-d
+fi
